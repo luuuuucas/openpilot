20c8eeb4469492e760a1e8cfd45b731e399b257b
Gijs Koning <gijs-koning@live.nl>
2022-05-17 14:05:31 +0200

    cleanup

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 from cereal import log, messaging
 from laika import AstroDog
-from laika.gps_time import GPSTime
 from laika.helpers import ConstellationId
 from laika.ephemeris import convert_ublox_ephem
 from laika.raw_gnss import GNSSMeasurement, calc_pos_fix, correct_measurements, process_measurements, read_raw_ublox
 
-time_first_gnss_message = None
-
 
 def process_ublox_msg(ublox_msg, dog: AstroDog, ublox_mono_time: int, correct=False):
-  global time_first_gnss_message
   if ublox_msg.which == 'measurementReport':
     report = ublox_msg.measurementReport
     if len(report.measurements) == 0:
       return None
     new_meas = read_raw_ublox(report)
-    if time_first_gnss_message is None:
-      time_first_gnss_message = GPSTime(report.gpsWeek, report.rcvTow)
     measurements = process_measurements(new_meas, dog)
     if len(measurements) == 0:
       return None
@@ -42,7 +36,9 @@ def process_ublox_msg(ublox_msg, dog: AstroDog, ublox_mono_time: int, correct=Fa
   elif ublox_msg.which == 'ephemeris':
     ephem = convert_ublox_ephem(ublox_msg.ephemeris)
     dog.add_ephem(ephem, dog.orbits)
-  # elif ublox_msg.which == 'ionoData': # todo add this. Needed to correct messages offline. First fix ublox_msg.cc to sent them.
+  # elif ublox_msg.which == 'ionoData':
+  # todo add this. Needed to correct messages offline. First fix ublox_msg.cc to sent them.
+  #  This is needed to correct the measurements
 
 
 def create_measurement_msg(meas: GNSSMeasurement):
@@ -72,7 +68,6 @@ def main():
   while True:
     sm.update()
 
-    # Todo if no internet available use latest ephemeris
     if sm.updated['ubloxGnss']:
       ublox_msg = sm['ubloxGnss']
       msg = process_ublox_msg(ublox_msg, dog, sm.logMonoTime['ubloxGnss'])
